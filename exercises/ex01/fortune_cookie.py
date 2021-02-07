81b9043488c3c27b4d149fc970ddfa82260454ce
Shaye Backus <Shayebac@live.unc.edu>
2021-02-07 15:57:50 -0500

    Fixed syntac error

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730309878"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,19 @@ from random import randint
 
 
 # Begin your solution here...
+fortune = (randint(1, 100))
+
+print("Your fortune cookie says...")
+
+if fortune <= 50:
+    if fortune <= 25:
+        print("You will receieve good news very soon.")
+    else:
+            print("Luck is coming your way.")
+else:
+    if fortune <= 75:
+        print("Your biggest dream will soon come true.")
+    else: 
+            print("Your hard work will soon pay off.")
+
+print("Now, go spread positive vibes!")                   
