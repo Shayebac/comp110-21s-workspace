ddd3f28a90655958f6be86133e671782052be892
Shaye Backus <Shayebac@live.unc.edu>
2021-02-11 21:14:13 -0500

    revised

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -6,17 +6,16 @@ __author__ = "730309878"
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
-    print(tar_heels(choice = int))
+    print(tar_heels(choice))
 
-def tar_heels(val: int) -> str:
-    val = input("Enter an int: ")
+def tar_heels(choice: int) -> str:
+    """Definition of the function."""
 
-    if int(val) % 2 == 0 and int(val) % 7 == 0:
+    if int(choice) % 2 == 0 and int(choice) % 7 == 0:
         return ("TAR HEELS")
-    elif int(val) % 2 == 0:
+    elif int(choice) % 2 == 0:
         return ("TAR")
-    elif int(val) % 7 == 0:
+    elif int(choice) % 7 == 0:
         return ("HEELS")
     else:
         return ("CAROLINA")
