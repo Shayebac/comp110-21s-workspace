eac4fc23bad4a25cb04e6149e02e498a31438723
Shaye Backus <Shayebac@live.unc.edu>
2021-03-07 14:39:24 -0500

    ex03

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,13 +1,28 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730309878"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(avoid_fifth("hello there"))
+    print(avoid_fifth("hello"))
+    print(avoid_fifth("The sentence we have here possesses E's galore!"))
+   
 
+def avoid_fifth(words: str) -> str:
+    """Removes e and E from all strings."""
+    i: int = 0
+    while i < len(words):
+        if words[i] == "e":
+            words.pop(i)
+            i += 1
+        elif words[i] == "E": 
+            words.pop(i)
+            i += 1 
+        else:
+            i += 1
+    return words
 
+        
 if __name__ == "__main__":
     main()
--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,24 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730309878"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+    print(palindromify("han", True))
+    print(palindromify("red", True))
+    print(palindromigy("live on time", False))
+
+
+def palindromify(x: str, y: bool) -> str:
+    """Creates palindrome out of given string."""
+    i: int = 0
+    while i < len(x):
+        if len(x) % 2 == 0:
+
+        else:
+    return x   
 
 
 if __name__ == "__main__":
