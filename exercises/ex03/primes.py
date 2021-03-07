bc01efa0433fa0badbeac0e5a02fbe629f42df74
Shaye Backus <Shayebac@live.unc.edu>
2021-03-07 17:25:53 -0500

    ex03

--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -6,12 +6,24 @@ __author__: str = "730309878"
 def main() -> None:
     """Entrypoint of the program."""
     print(is_prime(5))
-    print(list_primes(10, 20))
+    print(list_primes(3, 7))
 
 
 def is_prime(x: int) -> bool:
+    """Check prime number."""
+    if x > 1:
+        for i in range(2, x):
+            if (x % i) == 0:
+                return False
+        else:
+            return True 
+    else:
+        return False
 
-def list_primes(y: int, z:int) -> list[int]:
+
+def list_primes(x: int, y: int) -> list[int]:
+    """Check for prime numbers in list."""
+    
 
 if __name__ == "__main__":
     main()
