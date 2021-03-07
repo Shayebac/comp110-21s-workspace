eb3e5fda6ddcf0fe04c20490af3cfeb924f3f461
Shaye Backus <Shayebac@live.unc.edu>
2021-03-07 16:28:16 -0500

    03

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -14,15 +14,13 @@ def main() -> None:
 def palindromify(word: str, even_odd: bool) -> str:
     """Creates palindrome out of given string."""
     i: int = 0
-    stringlength: int = len(word)
     new_word: str = ""
     while i < len(word):
-        # word is even, statement is true 
-        if len(word) % 2 != 0:
+        if len(word) % 2 == 0:
+            
             i += 1
-        # word is odd, statement is false 
         else:
-            new_word = (word + " " + (word[::-1]))
+            new_word = (word + (word[::-1]))
             i += 1
     return new_word
 
--- exercises/ex03/primes.py
+++ exercises/ex03/primes.py
@@ -1,13 +1,17 @@
 """EX03 - prime functions."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730309878"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(is_prime(5)), print(list_primes(10, 20))
+    print(is_prime(5))
+    print(list_primes(10, 20))
 
+
+def is_prime(x: int) -> bool:
+
+def list_primes(y: int, z:int) -> list[int]:
 
 if __name__ == "__main__":
     main()
